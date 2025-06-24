dd3bdafb3f289e595315b124573ff553b9ebe23e
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-06-24 15:13:57 +0530

    Implement thunk for currency conversion and update customer slice

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { act } from "react";
 
 const initialState = {
   balance: 0,
@@ -14,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -38,12 +38,31 @@ const accountSlice = createSlice({
       state.loan = 0;
       state.loanPurpose = "";
     },
+    ConvertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
 console.log(accountSlice);
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API call
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`
+    );
+    const data = await res.json();
+
+    const converted = Number((amount * data.rates.USD).toFixed(2));
+    dispatch({ type: "account/deposit", payload: converted });
+    // return action
+  };
+}
 
 export default accountSlice.reducer;
 
